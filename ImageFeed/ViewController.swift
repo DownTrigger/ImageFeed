1618d7b89b946d809a42fcd40df70e49fd1746b3
DownTrigger <andrew.ruzavin@gmail.com>
2025-10-30 17:28:32 +0700

    Initial project setup

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Рузавин on 30/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
