d875f28b6102d1758db374bad28c699f150cb834
DownTrigger <andrew.ruzavin@gmail.com>
2025-11-04 16:00:45 +0700

    Add ImagesList screen with custom cell and dynamic height

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Андрей Рузавин on 31/10/25.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
