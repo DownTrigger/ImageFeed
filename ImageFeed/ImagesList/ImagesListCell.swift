0e1113ae399317e5013a6c6d981c47b9f0ac5cd9
DownTrigger <andrew.ruzavin@gmail.com>
2025-11-04 17:16:35 +0700

    Add Russian localization and gradient background to ImagesListCell

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -14,5 +14,27 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var gradientView: UIView!
     
+    private let gradientLayer = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = gradientView.bounds
+    }
+    
+    private func setupGradient() {
+        guard let blackColor = UIColor(named: "YP Black") else { return }
+        
+        gradientLayer.colors = [blackColor.withAlphaComponent(1).cgColor, blackColor.withAlphaComponent(0).cgColor]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
