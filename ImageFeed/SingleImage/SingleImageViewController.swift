da62629313d63c543b0a53c177e0145a1f080780
DownTrigger <andrew.ruzavin@gmail.com>
2025-12-01 12:07:16 +0700

    Add Share button and improve image zoom/centering

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -8,17 +8,82 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
+    // MARK: Properties
+    
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
+            
             imageView.image = image
+            imageView.frame.size = image.size
         }
     }
     
+    @IBOutlet private var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
+    
+    // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard let image = imageView.image else { return }
+        updateMinZoomScale(for: image)
+        centerImage()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        guard let image else { return }
+        let share = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(share, animated: true, completion: nil)
+    }
+    
+    // MARK: Private Methods
+    
+    private func updateMinZoomScale(for image: UIImage) {
+        let widthScale = scrollView.bounds.width / image.size.width
+        let heightScale = scrollView.bounds.height / image.size.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = minScale * 3
+        scrollView.zoomScale = minScale
+    }
+    
+    private func centerImage() {
+        let scrollSize = scrollView.bounds.size
+        let imageSize = imageView.frame.size
+        
+        let verticalInset = max(0, (scrollSize.height - imageSize.height) / 2)
+        let horizontalInset = max(0, (scrollSize.width - imageSize.width) / 2)
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalInset, left: horizontalInset, bottom: verticalInset, right: horizontalInset)
+    }
+}
+
+// MARK: UIScrollViewDelegate
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerImage()
     }
 }
